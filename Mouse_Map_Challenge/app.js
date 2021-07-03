07938ab10a21164ea3c0bb451b0e42b15fcfd0de
John (Jef) Fisher <Fishe545@gmail.com>
2021-07-03 09:04:44 +0800

    mouse map challenge finished

---
+++ Mouse_Map_Challenge/app.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener('mousemove', function(e) { 
+
+    const red = Math.round((e.pageX / window.innerWidth) * 255);
+    
+    const green = Math.round((e.pageY / window.innerHeight) * 255);
+
+    const color = `rgb(${red}, ${green}, 0)`;
+
+    document.body.style.backgroundColor = color;
+})
